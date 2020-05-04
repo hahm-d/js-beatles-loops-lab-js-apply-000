c5a2ada64d22207b678b827497a3ff4e172a5935
danny <ohdangitsdaniel@gmail.com>
2020-05-04 19:38:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function iLoveTheBeatles(num){
     output.push("I love the Beatles!");
     counter++;
   }
-  while(counter <= (num - 15));
+  while(counter < 15 && output.length == num - 15);
   
   return output;
 }
