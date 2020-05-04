dfd92a6bcec53d80bd596bf757857bf2d0c7e572
danny <ohdangitsdaniel@gmail.com>
2020-05-04 19:14:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeatlesPlay(arr1, arr2){
+  let output = [];
+  
+  for(let i=0; i < arr1.length; i++){
+    let temp = arr1[i] + arr2[i];
+    output.push(temp);
+  }
+  return output;
+}
