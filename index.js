eaa6e6a7ffefdad0ed99ddee9e92c79cc46aab21
danny <ohdangitsdaniel@gmail.com>
2020-05-04 19:16:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(arr1, arr2){
   let output = [];
   
   for(let i=0; i < arr1.length; i++){
-    let temp = arr1[i] + arr2[i];
+    let temp = arr1[i] + " plays " + arr2[i];
     output.push(temp);
   }
   return output;
